2ccf2879cc7741f018e412b510218fa2c6ec9c1a
Mzamo <molimica@gmail.com>
2017-04-18 20:03:31 +0200

    Menu implementado, el play y el exit

---
+++ Assets/Code/butt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class butt : MonoBehaviour {
+
+	#region Public Attributes
+	#endregion
+	
+	#region Private Attributes
+	#endregion
+	
+	#region typedef
+	#endregion
+
+	#region Properties
+	#endregion
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void ButtonPlay()
+	{
+		Application.LoadLevel("Preload");
+	}
+
+	public void ButtonExit()
+	{
+		Application.Quit();
+	}
+}
